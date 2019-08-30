a6fa2c7f4c043194cca71632480fb17442cb0bdd
markgreene74 <g.cunsolo3@gmail.com>
2019-08-30 13:09:21 +0000

    Define some structure

--- game-of-life/game.py
+++ game-of-life/game.py
@@ -2,3 +2,9 @@ import datetime as dt
 
 def when_is_now():
     return dt.datetime.now()
+
+def run(BOARD_SIZE, CYCLES, DRYRUN):
+    ''' docstring goes here'''
+    print(f'This is the board size: {BOARD_SIZE}')
+    print(f'This is the number of cycles: {CYCLES}')
+    print(f'This is the value of DRYRUN: {DRYRUN}')
--- game-of-life/main.py
+++ game-of-life/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import game
+
+# read env variables
+BOARD_SIZE = os.environ.get('BOARD_SIZE', '20x20')
+CYCLES = os.environ.get('CYCLES')
+DRYRUN = os.environ.get('DRYRUN', False)
+
 
 if __name__ == "__main__":
     print("{}".format(game.when_is_now()))
+    game.run(BOARD_SIZE, CYCLES, DRYRUN)
     print("hello world")
