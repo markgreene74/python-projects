76c1e7cc820b1c9e05a61fe016b28de353efdc3d
markgreene74 <g.cunsolo3@gmail.com>
2019-11-15 16:58:13 +0000

    first stab at plays data

--- rock-paper-scissors-lizard-spock/rpsls.py
+++ rock-paper-scissors-lizard-spock/rpsls.py
@@ -1,8 +1,18 @@
 #!/bin/env python3
 
 import os
+from collections import namedtuple
 
 ROUNDS = int(os.environ.get("RPSLS_ROUNDS", 10))
+Play = namedtuple("Play", "name, winover")
+plays_list = ["rock", "paper", "scissors", "lizard", "spock"]
+wins_list = [[1, 2, 3] for i in range(5)]
+all_plays = [Play(name=i, winover=j) for i, j in zip(plays_list, wins_list)]
+
+
+def play(aplay=None):
+    if not aplay:
+        return all_plays
 
 
 def main():
