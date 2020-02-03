30f258b71bd03b3d3318541783693d908dc13d5e
markgreene74 <g.cunsolo3@gmail.com>
2020-02-03 22:57:38 +0000

    replaced namedtupls with dict

--- rock-paper-scissors-lizard-spock/tests.py
+++ rock-paper-scissors-lizard-spock/tests.py
@@ -2,20 +2,17 @@ import pytest
 
 import rpsls
 
-from collections import namedtuple
-
-Play = namedtuple("Play", "name, winover")
-test_all_plays_data = [
-    Play(name="rock", winover=[1, 2, 3]),
-    Play(name="paper", winover=[1, 2, 3]),
-    Play(name="scissors", winover=[1, 2, 3]),
-    Play(name="lizard", winover=[1, 2, 3]),
-    Play(name="spock", winover=[1, 2, 3]),
-]
+test_all_plays_data = {
+    "rock": "scissors,lizard",
+    "paper": "rock,spock",
+    "scissors": "paper,lizard",
+    "lizard": "spock,paper",
+    "spock": "scissors,rock"
+}
 test_single_plays_data = [
-    ("rock", [Play(name="rock", winover=[1, 2, 3])]),
-    ("scissors", [Play(name="scissors", winover=[1, 2, 3])]),
-    ("spock", [Play(name="spock", winover=[1, 2, 3])]),
+    ("rock", {"rock": "scissors,lizard"}),
+    ("scissors", {"scissors": "paper,lizard"}),
+    ("spock", {"spock": "scissors,rock"}),
 ]
 
 
@@ -36,4 +33,4 @@ def test_single_plays(arg, expected):
 
 
 def test_single():
-    assert rpsls.play("spock") == [Play(name="spock", winover=[1, 2, 3])]
+    assert rpsls.play("spock") == {"spock": "scissors,rock"}
